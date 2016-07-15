33c5fcdfbb2b9ac1383d908732f24f0a23e8ac3a
tararee <tararee@gmail.com>
2016-07-15 00:30:21 -0400

    Add files via upload

---
+++ stufficantfind.js
@@ -0,0 +1,18 @@
+var thing = [clearSunglasses, debitCard, featherRing, greenPen, iphoneCharger, keys, mouthGuard, nasalSpray, orangeSunglasses, remoteControl];
+
+
+
+
+var clearSunglasses = { lost : true, img.src:"img/clearSunglasses.jpg" };
+var debitCard = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var featherRing = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var greenPen = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var iphoneCharger = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var keys = {lost : false, img.src:"img/clearSunglasses.jpg"};
+var mouthGuard = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var nasalSpray = {lost : true, img.src:"img/clearSunglasses.jpg"};
+var orangeSunglasses = {lost : false, img.src:"img/clearSunglasses.jpg"};
+var remoteControl = {lost : true, img.src:"img/clearSunglasses.jpg"};
+
+if (thing = true){console.log(thing)};
+
